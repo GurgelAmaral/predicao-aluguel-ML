92b5a8763f28fa0c39b1e4f49f1ab37c8e28bbb7
GurgelAmaral <monsieurlambertz@gmail.com>
2025-05-29 02:04:06 -0300

    adição da lógica do pipeline final e correção do features.py

--- src/features.py
+++ src/features.py
@@ -1,7 +1,11 @@
+import sys
+sys.path.append('data')
 '''
     columns and types selection
 '''
+import load_data as ld
 
 #função que captura as colunas numéricas e categóricas
-def get_num_col_features(df):
-    return df.select_dtypes(include=["number"]).columns, df.select_dtypes(include=["object", "string"]).columns
+
+def get_num_col_features(df=ld.load_data()):
+    return df.select_dtypes(include=["number"]).columns.to_list(), df.select_dtypes(include=["object", "string"]).columns.to_list()
--- src/pipeline.py
+++ src/pipeline.py
@@ -1,3 +1,33 @@
+from sklearn.pipeline import Pipeline
+from sklearn.linear_model import Ridge
+from sklearn.compose import ColumnTransformer
+from sklearn.preprocessing import FunctionTransformer
+import preprocessing as pr
+import features
 '''
     final model pipeline
 '''
+
+
+
+built_num_pipeline = pr.build_num_pipeline()
+built_cat_pipeline = pr.build_cat_pipeline()
+
+predefined_num_cols, predefined_cat_cols = features.get_num_col_features()
+
+def build_final_pipeline(num_pipeline=built_num_pipeline, cat_pipeline=built_cat_pipeline, num_cols=predefined_num_cols, cat_cols=predefined_cat_cols ):
+
+    prep = ColumnTransformer(
+        transformers=[
+            ('num', num_pipeline, num_cols),
+            ('cat', cat_pipeline, cat_cols)
+        ]
+    )
+
+    final_pipeline = Pipeline([
+        ('negotiation_type_checker', FunctionTransformer(pr.fix_operation_type)),
+        ('prep', prep),
+        ('model', Ridge())
+    ])
+
+    return final_pipeline
